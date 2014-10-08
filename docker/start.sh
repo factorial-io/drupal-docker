8952b850fab183a6330611eacadd3aaa2ef49f8c
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2014-10-08 14:35:51 +0200

    reaordering mysql-stuff (borrowed from https://github.com/sameersbn/docker-mysql)

--- docker/start.sh
+++ docker/start.sh
@@ -1,9 +1,42 @@
 #!/bin/bash
 
+if [ ! -d /var/lib/mysql/mysql ]; then
+  echo "Installing database..."
+  mysql_install_db --user=mysql >/dev/null 2>&1
+
+  # start mysql server
+  echo "Starting MySQL server..."
+  /usr/bin/mysqld_safe >/dev/null 2>&1 &
+
+  # wait for mysql server to start (max 30 seconds)
+  timeout=30
+  while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
+  do
+    timeout=$(($timeout - 1))
+    if [ $timeout -eq 0 ]; then
+      echo "Could not connect to mysql server. Aborting..."
+      exit 1
+    fi
+    echo "Waiting for database server to accept connections..."
+    sleep 1
+  done
+
+  ## create a localhost only, debian-sys-maint user
+  ## the debian-sys-maint is used while creating users and database
+  ## as well as to shut down or starting up the mysql server via mysqladmin
+  echo "Creating debian-sys-maint user..."
+  mysql -uroot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;"
+
+  mysqladmin -u root password admin
+  mysqladmin -u root --password=admin shutdown
+  sleep 10s
+
+fi
+
+
 if [ ! -f "${WEB_ROOT}/sites/default/settings.php" ]; then
 
   # Start mysql
-  mysql_install_db
   /usr/bin/mysqld_safe &
   sleep 10s
   # Generate passwords
