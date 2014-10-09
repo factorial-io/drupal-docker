501fea340e855f69104bb99df3e3f52feeb3ad07
Stephan Maximilian Huber <hello@stephanmaximilianhuber.com>
2014-10-09 11:45:59 +0200

    fix mysql-setup logic

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d /var/lib/mysql/mysql ]; then
+if [ ! -f /mysql_is_installed ]; then
   echo "Installing database..."
   mysql_install_db --user=mysql >/dev/null 2>&1
 
@@ -8,29 +8,12 @@ if [ ! -d /var/lib/mysql/mysql ]; then
   echo "Starting MySQL server..."
   /usr/bin/mysqld_safe >/dev/null 2>&1 &
 
-  # wait for mysql server to start (max 30 seconds)
-  timeout=30
-  while ! /usr/bin/mysqladmin -u root status >/dev/null 2>&1
-  do
-    timeout=$(($timeout - 1))
-    if [ $timeout -eq 0 ]; then
-      echo "Could not connect to mysql server. Aborting..."
-      exit 1
-    fi
-    echo "Waiting for database server to accept connections..."
-    sleep 1
-  done
-
-  ## create a localhost only, debian-sys-maint user
-  ## the debian-sys-maint is used while creating users and database
-  ## as well as to shut down or starting up the mysql server via mysqladmin
-  echo "Creating debian-sys-maint user..."
-  mysql -uroot -e "GRANT ALL PRIVILEGES on *.* TO 'debian-sys-maint'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;"
+  sleep 10s
 
   mysqladmin -u root password admin
   mysqladmin -u root --password=admin shutdown
   sleep 10s
-
+  touch /mysql_is_installed
 fi
 
 
@@ -43,10 +26,7 @@ if [ ! -f "${WEB_ROOT}/sites/default/settings.php" ]; then
   DRUPAL_DB="drupal"
   MYSQL_PASSWORD='admin'
   DRUPAL_PASSWORD='admin'
-  # This is so the passwords show up in logs.
-  echo mysql root password: $MYSQL_PASSWORD
-  echo drupal password: $DRUPAL_PASSWORD
-  mysqladmin -u root password $MYSQL_PASSWORD
+
 
   if [ -n "${NO_INSTALL}" ]; then
     echo "skipping drupal setup, please setup manually."
